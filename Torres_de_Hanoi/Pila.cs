060431b973d36d54fed03f0753dab91387ff7abd
Alejandro Roca Artiga <AROCART@upv.edu.es>
2023-03-13 19:52:46 +0100

    Clase pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,35 +10,54 @@ namespace Torres_de_Hanoi
     class Pila
     {
         public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
         public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
-        public List<Disco> Elementos { get; set; }
-        */
+        public List<Disco> Elementos { get; set; } = new List<Disco>();
 
-        /* TODO: Implementar métodos */
+
+        public Pila(int n)
+        {
+            this.Size = n;
+            for (int i = n; i > 0; i--)
+            {
+                Disco disco = new Disco();
+                disco.Valor = i;
+                this.Elementos.Add(disco);
+            }
+            this.Top = this.Elementos[this.Size - 1].Valor;
+        }
+
         public Pila()
         {
-
+            this.Size = 0;
+            this.Top = 0;
         }
 
         public void push(Disco d)
         {
-
+            this.Size++;
+            this.Top = d.Valor;
+            this.Elementos.Add(d);
         }
-
         public Disco pop()
         {
-            return null;
-        }                
-
+            Disco disco = new Disco();
+            disco = this.Elementos[this.Size - 1];
+            this.Elementos.Remove(disco);
+            this.Size--;
+            if (this.Size > 0)
+                this.Top = this.Elementos[this.Size - 1].Valor;
+            return disco;
+        }
         public bool isEmpty()
         {
-            return true;
+            if (this.Size == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
-
     }
 }
